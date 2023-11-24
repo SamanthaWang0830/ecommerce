f85fbb6b8f1f8a47c6858b4a20e99b75648e2618
zijingw <109635814+SamanthaWang0830@users.noreply.github.com>
2023-11-24 14:23:52 -0500

    try to fix

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -9,12 +9,12 @@ import { getStockCount } from "@/actions/getStockCount";
 import { Overview } from "@/components/overview";
 import { getGraphRevenue } from "@/actions/getGraphRevenue";
 
-interface DashboardPage{
+interface DashboardPageProps{
     params:{storeId: string}
 }
 
- {/* @ts-expect-error Server Component */}
-const DashboardPage: React.FC<DashboardPage>=async({params})=>{
+
+const DashboardPage: React.FC<DashboardPageProps>=async({params})=>{
 
     const totalRevenue=await getTotalRevenue(params.storeId)
     const salesCount= await getSalesCount(params.storeId)
